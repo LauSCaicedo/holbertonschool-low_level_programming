89a3fe65eae9d026f1efd5cf0f018954f9c7953f
LauSCaicedo <3383@holbertonschool.com>
2021-06-21 21:11:04 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Print Alphabet
+*
+*  Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int letter = 97;
+int letterA = 65;
+while (letter <= 122)
+{
+putchar(letter);
+letter++;
+}
+while (letterA <= 90)
+{
+putchar(letterA);
+letter++;
+}
+putchar('\n');
+return (0);
+}
