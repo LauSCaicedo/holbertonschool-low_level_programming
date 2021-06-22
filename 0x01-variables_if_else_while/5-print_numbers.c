2914be0648acc192bc51c2e6e3d3540b41912d91
LauSCaicedo <3383@holbertonschool.com>
2021-06-22 11:43:28 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print Alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+printf("%i", num);
+num++;
+}
+putchar('\n');
+return (0);
+}
