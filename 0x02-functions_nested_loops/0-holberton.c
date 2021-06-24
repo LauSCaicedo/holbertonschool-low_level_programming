85e9a92f5900bb758c4d42ec967ee7c5288f8630
LauSCaicedo <3383@holbertonschool.com>
2021-06-23 19:29:22 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - _putchar
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char paragraph[]= "Holberton";
+size_t i = 0;
+size_t arrayLength = strlen(paragraph);
+for (i = 0; i < arrayLength; i++)
+{
+putchar(paragraph[i]);
+}
+putchar('\n');
+return (0);
+}
