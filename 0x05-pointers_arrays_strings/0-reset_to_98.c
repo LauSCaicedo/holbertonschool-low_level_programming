0287ee0edd649cfb46a22118a1a6dc8e3e978c05
LauSCaicedo <3383@holbertonschool.com>
2021-06-29 20:36:38 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Whits this function we will assign values.
+ * @n: Pointer to integer
+ * Return: Nothing.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
