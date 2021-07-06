4ffd59dcb31d68a2e3cad9b1123e471a18e6eadb
LauSCaicedo <3383@holbertonschool.com>
2021-07-06 16:56:20 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - Function for localizacted a character.
+ * @s: Pointer with the place of work.
+ * @c: variable for caracter.
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+int a;
+for (a = 0; s[a] != '\0'; a++)
+{
+if (s[a] == c)
+{
+return (s + a);
+}
+}
+return ('\0');
+}
