4b92b789c4aca68f15a8303b4ab6dbea462b2525
LauSCaicedo <3383@holbertonschool.com>
2021-07-06 20:35:08 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,17 @@
 
 char *_strchr(char *s, char c)
 {
-int a;
-for (a = 0; s[a] != '\0'; a++)
-{
-if (s[a] == c)
-{
-return (s + a);
-}
-}
-return (0);
+	int a, x = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (x <= a)
+		{
+		if (s[a] == c)
+		{
+			return (s + a);
+		}
+		}
+	}
+	return (0);
 }
