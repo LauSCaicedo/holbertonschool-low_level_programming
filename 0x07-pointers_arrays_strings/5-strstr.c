0d2cc1a1e8744e523b8b2e8bccf90c264d8c9f24
LauSCaicedo <3383@holbertonschool.com>
2021-07-07 21:55:12 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strstr - Function for locates a substring.
+ * @haystack: Pointer one.
+ * @needle: Pointer two.
+ * Return: Needle or 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int x, z;
+
+	for (x = 0; haystack[x] != '\0'; x++)
+	{
+		for (z = 0; needle[z] != '\0'; z++)
+		{
+			if (haystack[x] == needle[z])
+			{
+				return (needle);
+			}
+		}
+	}
+	return (0);
+}
