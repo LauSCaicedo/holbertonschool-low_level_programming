5b7c9e41cae3374ee63814509bdc92c471567b58
LauSCaicedo <3383@holbertonschool.com>
2021-07-10 22:24:35 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int x, z, cont;
+	int x;
 
-	for (x = 0; s1[x] != 0; ++x)
+	for (x = 0; *s1 && *s2 && *s1 == *s2; x++)
 	{
-		for (z = 0; s2[z] != 0; ++z)
-		{
-			if (s1[x] != s2[z])
-			{
-				cont = s1[x] - s2[z];
-				return (cont);
-			}
-			else
-			{
-				return (0);
-			}
-		}
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
