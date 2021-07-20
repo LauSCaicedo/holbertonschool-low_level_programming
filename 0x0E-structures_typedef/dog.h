441d64116222b37f22b32988e2a7afb602f86c2d
LauSCaicedo <3383@holbertonschool.com>
2021-07-19 19:22:14 -0500

    Structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Structure to define the data of a dog.
+ * @name: Name a dog.
+ * @age: Age a dog.
+ * @owner: owner of a dog.
+ * Return: Always 0.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
