5d1cd70fe33413504930727af11c28ad403f0ad1
LauSCaicedo <3383@holbertonschool.com>
2021-07-21 01:01:41 -0500

    Pointers to functions

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_name - Function that point to other pointer.
+ * @name: Name of the person.
+ * @f: Pointer to function.
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
