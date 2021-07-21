bcbb6237fc9c8671396ddcf38321fcc917bb5083
LauSCaicedo <3383@holbertonschool.com>
2021-07-21 15:44:36 -0500

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,22 +1,53 @@
 #include "3-calc.h"
 
-int op_add(int Number1, int Number2)
+/**
+ * op_add - Function for add.
+ * @number1: First number.
+ * @number2: Second number.
+ * Return: Always 0.
+ */
+
+int op_add(int number1, int number2)
 {
-    return (Number1 + Number2);
+	return (number1 + number2);
 }
-int op_sub(int Number1, int Number2)
+/**
+ * op_sub - Function for subtract.
+ * @number1: First number.
+ * @number2: Second number.
+ * Return: Always 0.
+ */
+int op_sub(int number1, int number2)
 {
-    return (Number1 - Number2);
+	return (number1 - number2);
 }
-int op_mul(int Number1, int Number2)
+/**
+ * op_mul - Function for multiply.
+ * @number1: First number.
+ * @number2: Second number.
+ * Return: Always 0.
+ */
+int op_mul(int number1, int number2)
 {
-    return (Number1 * Number2);
+	return (number1 * number2);
 }
-int op_div(int Number1, int Number2)
+/**
+ * op_div - Function for divide.
+ * @number1: First number.
+ * @number2: Second number.
+ * Return: Always 0.
+ */
+int op_div(int number1, int number2)
 {
-    return (Number1 / Number2);
+	return (number1 / number2);
 }
-int op_mod(int Number1, int Number2)
+/**
+ * op_mod - Function for get the modulus of a number.
+ * @number1: First number.
+ * @number2: Second number.
+ * Return: Always 0.
+ */
+int op_mod(int number1, int number2)
 {
-    return (Number1 % Number2);
+	return (number1 % number2);
 }
