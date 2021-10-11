77dd57e6cb4bd48f3a68100348d1338ab7f18bd3
LauSCaicedo <3383@holbertonschool.com>
2021-10-10 23:47:19 -0500

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -23,7 +23,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	newhasht->size = size;
 	newhasht->array = malloc(sizeof(hash_table_t *) * size);
 	if (newhasht->array == NULL)
 	{
