bae4044fd389d362e14b40c5649d680769960509
LauSCaicedo <3383@holbertonschool.com>
2021-10-11 00:09:35 -0500

    Hash Tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,13 @@
-#include "hashtable.h"
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Function that retrieves a value associated with a key.
+ * @ht: Is the hash table you want to look into.
+ * @key: Is the key you are looking for.
+ * Return: The value associated with the element,
+ * or NULL if key couldn’t be found.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+}
