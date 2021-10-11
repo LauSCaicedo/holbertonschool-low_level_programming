e388b1285cd7611d2167e15a5b60a0f4ae4ba961
LauSCaicedo <3383@holbertonschool.com>
2021-10-11 00:06:08 -0500

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,12 @@
-#include "hashtable.h"
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Function that adds an element to the hash table.
+ * @ht: Is the hash table you want to add or update the key/value to.
+ * @key: Is the key.
+ * @value: Is the value associated with the key.
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
