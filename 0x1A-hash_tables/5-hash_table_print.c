b42c15baaca075e9c1c362a585cf85a890bd14a3
LauSCaicedo <3383@holbertonschool.com>
2021-10-11 00:14:57 -0500

    Hash Tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,1 +1,11 @@
-#include "hashtable.h"
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Function that prints a hash table.
+ * @ht: Is the hash table.
+ * Return: Nothing.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+}
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,1 +1,11 @@
-#include "hashtable.h"
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Function that deletes a hash table.
+ * @ht: Is the hash table.
+ * Return: Nothing.
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+}
